7c4269c1b3ccf3a96b570be83c6b8c836f437bcf
Ramon Valdez <jr616934@gmail.com>
2021-08-18 13:56:16 -0600

    nombre servicio reporte

--- Demo/Models/ReporteRendimientoEquiposModelView.cs
+++ Demo/Models/ReporteRendimientoEquiposModelView.cs
@@ -14,6 +14,7 @@ namespace Demo.Models
         public DateTime FechaInicial { get; set; }
         public DateTime FechaFinal { get; set; }
         public string servicio { get; set; }
+        public string nombre { get; set; }
         public List<RendimientoEquiposServicios> Datos { get; set; }
     }
 }
